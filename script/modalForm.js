6e70204f0a6cbd1ab1333a95f5ef627ddc7beb34
akademuk <akademuk24@gmail.com>
2023-05-10 12:07:42 +0300

    sss

---
+++ script/modalForm.js
@@ -0,0 +1,19 @@
+let modalForm = document.querySelector('.modalForm'); 
+let modalFormContent = document.querySelector('.modalFormContent'); 
+let modalFormContentOpen = document.querySelectorAll('.modalFormContent-open');
+let modalFormContentClose = document.querySelector('.modalFormContent-close'); 
+let body1 = document.querySelector('.body'); 
+
+modalFormContentOpen.forEach((button) => { 
+    button.addEventListener('click', (e) => { 
+        e.preventDefault(); 
+        modalForm.classList.add('active'); 
+        modalFormContent.classList.add('active');
+        body1.classList.add('active');
+    })
+});
+modalFormContentClose.addEventListener('click',() => { 
+    modalForm.classList.remove('active'); 
+    modalFormContent.classList.remove('active'); 
+    body1.classList.remove('active');
+});
