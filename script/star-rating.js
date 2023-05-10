5acfd0d4735d053780ff7083e3ffaf35e2db2380
akademuk <akademuk24@gmail.com>
2023-05-10 11:57:05 +0300

    sss

---
+++ script/star-rating.js
@@ -0,0 +1,82 @@
+
+$(".my-rating-1").starRating({
+    initialRating: 3.5,
+    disableAfterRate: false,
+    onHover: function (currentIndex, currentRating, $el) {
+        $('.live-rating1').text(currentIndex);
+    },
+    onLeave: function (currentIndex, currentRating, $el) {
+        $('.live-rating1').text(currentRating);
+    }
+});
+$(".my-rating-2").starRating({
+    initialRating: 3.5,
+    disableAfterRate: false,
+    onHover: function (currentIndex, currentRating, $el) {
+        $('.live-rating2').text(currentIndex);
+    },
+    onLeave: function (currentIndex, currentRating, $el) {
+        $('.live-rating2').text(currentRating);
+    }
+});
+$(".my-rating-3").starRating({
+    initialRating: 3.5,
+    disableAfterRate: false,
+    onHover: function (currentIndex, currentRating, $el) {
+        $('.live-rating3').text(currentIndex);
+    },
+    onLeave: function (currentIndex, currentRating, $el) {
+        $('.live-rating3').text(currentRating);
+    }
+});
+$(".my-rating-4").starRating({
+    initialRating: 3.5,
+    disableAfterRate: false,
+    onHover: function (currentIndex, currentRating, $el) {
+        $('.live-rating4').text(currentIndex);
+    },
+    onLeave: function (currentIndex, currentRating, $el) {
+        $('.live-rating4').text(currentRating);
+    }
+});
+$(".my-rating-5").starRating({
+    initialRating: 3.5,
+    disableAfterRate: false,
+    onHover: function (currentIndex, currentRating, $el) {
+        $('.live-rating5').text(currentIndex);
+    },
+    onLeave: function (currentIndex, currentRating, $el) {
+        $('.live-rating5').text(currentRating);
+    }
+});
+$(".my-rating-6").starRating({
+    initialRating: 3.5,
+    disableAfterRate: false,
+    onHover: function (currentIndex, currentRating, $el) {
+        $('.live-rating6').text(currentIndex);
+    },
+    onLeave: function (currentIndex, currentRating, $el) {
+        $('.live-rating6').text(currentRating);
+    }
+});
+$(".my-rating-7").starRating({
+    initialRating: 3.5,
+    disableAfterRate: false,
+    onHover: function (currentIndex, currentRating, $el) {
+        $('.live-rating7').text(currentIndex);
+    },
+    onLeave: function (currentIndex, currentRating, $el) {
+        $('.live-rating7').text(currentRating);
+    }
+});
+
+$(".my-rating-8").starRating({
+    initialRating: 3.5,
+    disableAfterRate: false,
+    onHover: function (currentIndex, currentRating, $el) {
+        $('.live-rating8').text(currentIndex);
+    },
+    onLeave: function (currentIndex, currentRating, $el) {
+        $('.live-rating8').text(currentRating);
+    }
+});
