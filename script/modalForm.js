6ab91da9dccbd46bae0104857830936c3bf5bbab
akademuk <akademuk24@gmail.com>
2023-08-18 13:17:13 +0300

    sss

--- script/modalForm.js
+++ script/modalForm.js
@@ -4,6 +4,9 @@ let modalFormContentOpen = document.querySelectorAll('.modalFormContent-open');
 let modalFormContentClose = document.querySelector('.close-popup2'); 
 let body1 = document.querySelector('.body'); 
 
+let popupScrollForm = document.querySelector('.popupScrollForm'); 
+let popupScroll2Form = document.querySelector('.popupScroll2Form'); 
+
 modalFormContentOpen.forEach((button) => { 
     button.addEventListener('click', (e) => { 
         e.preventDefault(); 
@@ -16,4 +19,7 @@ modalFormContentClose.addEventListener('click',() => {
     modalForm.classList.remove('active'); 
     modalFormContent.classList.remove('active'); 
     body1.classList.remove('active');
+
+    popupScrollForm.classList.remove('active');
+    popupScroll2Form.classList.remove('active');
 });
